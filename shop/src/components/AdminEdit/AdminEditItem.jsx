7d1445b7a9019feebce1c5a5c74cfbdc68912cf8
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-12-22 13:16:02 +0400

    Add files via upload

--- shop/src/components/AdminEdit/AdminEditItem.jsx
+++ shop/src/components/AdminEdit/AdminEditItem.jsx
@@ -1,19 +1,35 @@
-import { useContext } from "react"
-import { MyContext } from "../../App"
+import { useContext, useState } from "react";
+import { MyContext } from "../../App";
+import EditForm from "./EditForm";
 
 function AdminEditItem(props) {
+  let [editActive, setEditActive] = useState(false);
 
-    let {dispatchProducts} = useContext(MyContext)
+  let { dispatchAdminEdit, stateAdminEdit } = useContext(MyContext);
 
-    return(
-        <li>
-           <p>{props.product_name}</p>
-           <p>{props.product_description}</p>
-           <p>{props.product_price}</p>
-           <img src={props.url} alt={props.product_name} />
-           <button onClick={() => dispatchProducts({type:'ADD TO BAG', payload: props})}>DELETE</button>
-        </li>
-    )
+  return (
+    <li>
+      <p>{props.product_name}</p>
+      <p>{props.product_description}</p>
+      <p>{props.product_price}</p>
+      <img src={props.url} alt={props.product_name} />
+      <input
+        type="checkbox"
+        checked={stateAdminEdit.ids.includes(props.id)}
+        onChange={() =>
+          dispatchAdminEdit({ type: "SELECT PRODUCT", payload: props.id })
+        }
+      />
+      <button
+        onClick={() => dispatchAdminEdit({ type: "DELETE", payload: props.id })}
+      >
+        DELETE
+      </button>
+      <button onClick={() => setEditActive(!editActive)}>SHOW EDIT FORM</button>
+
+      {editActive && <EditForm productObj={props} />}
+    </li>
+  );
 }
 
-export default AdminEditItem
+export default AdminEditItem;
